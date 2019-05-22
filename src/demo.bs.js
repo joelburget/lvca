99d91b9352f1d3bc1119b6292d43b9a3eee4d63e
Joel Burget <joelburget@gmail.com>
2019-05-22 09:02:35 +0900

    Types changes.

--- src/demo.bs.js
+++ src/demo.bs.js
@@ -2,21 +2,63 @@
 'use strict';
 
 var Block = require("bs-platform/lib/js/block.js");
-var Caml_builtin_exceptions = require("bs-platform/lib/js/caml_builtin_exceptions.js");
+var Belt_List = require("bs-platform/lib/js/belt_List.js");
+var Belt_MapString = require("bs-platform/lib/js/belt_MapString.js");
+
+var Abt = /* module */[];
+
+var Ast = /* module */[];
+
+var Core = /* module */[];
 
 function eval_core(core) {
-  if (core.tag) {
-    throw [
-          Caml_builtin_exceptions.match_failure,
-          /* tuple */[
-            "demo.ml",
-            67,
-            60
-          ]
-        ];
-  } else {
-    return /* Error */Block.__(1, ["TODO"]);
-  }
+  var _ctx = Belt_MapString.empty;
+  var _core = core;
+  while(true) {
+    var core$1 = _core;
+    var ctx = _ctx;
+    switch (core$1.tag | 0) {
+      case 0 : 
+          var v = core$1[0][0];
+          var match = Belt_MapString.get(ctx, v);
+          if (match !== undefined) {
+            return /* Ok */Block.__(0, [match]);
+          } else {
+            return /* Error */Block.__(1, ["Unbound variable " + v]);
+          }
+      case 1 : 
+          return /* Ok */Block.__(0, [core$1[0]]);
+      case 2 : 
+          var match$1 = core$1[0];
+          if (match$1.tag === 3) {
+            var argNames = match$1[0];
+            if (Belt_List.length(argNames) !== Belt_List.length(core$1[1])) {
+              return /* Error */Block.__(1, ["mismatched application lengths"]);
+            } else {
+              var ctx$prime = Belt_MapString.merge(ctx, Belt_MapString.fromArray(Belt_List.toArray(Belt_List.zip(argNames, /* [] */0))), (function (_k, v1, v2) {
+                      if (v2 !== undefined) {
+                        return v2;
+                      } else if (v1 !== undefined) {
+                        return v1;
+                      } else {
+                        return undefined;
+                      }
+                    }));
+              _core = match$1[1];
+              _ctx = ctx$prime;
+              continue ;
+            }
+          } else {
+            return /* Error */Block.__(1, ["TODO"]);
+          }
+      case 3 : 
+      case 4 : 
+          return /* Error */Block.__(1, ["TODO"]);
+      case 5 : 
+          return /* Error */Block.__(1, ["Found a metavar!"]);
+      
+    }
+  };
 }
 
 function intersperse_after(list, el) {
@@ -48,6 +90,9 @@ function intersperse_after(list, el) {
   }
 }
 
+exports.Abt = Abt;
+exports.Ast = Ast;
+exports.Core = Core;
 exports.eval_core = eval_core;
 exports.intersperse_after = intersperse_after;
 /* No side effect */
--- src/demo.ml
+++ src/demo.ml
@@ -1,6 +1,25 @@
 open Belt
 
-type test = | Test;;
+type valence =
+  | FixedValence    of string list * string
+  | VariableValence of string * string
+;;
+
+type arity =
+  | Arity of string list * valence list
+;;
+
+type operatorDef =
+  | OperatorDef of string * arity
+;;
+
+type sortDef =
+  | SortDef of string list * operatorDef list
+;;
+
+type language =
+  | Language of sortDef Belt.Map.String.t
+;;
 
 type primitive =
   | PrimInteger of Bigint.t
@@ -8,15 +27,29 @@ type primitive =
   | PrimBool    of bool
   ;;
 
-type scope =
-  | Scope of string list * term
+module Abt = struct
 
-and term =
-  | Term      of string * scope list
-  | Var       of string
-  | Sequence  of term list (* TODO: list vs array? *)
-  | Primitive of primitive
-  ;;
+  type scope =
+    | Scope of string list * t
+
+  and t =
+    | Term      of string * scope list
+    | Var       of string
+    | Sequence  of t list (* TODO: list vs array? *)
+    | Primitive of primitive
+    ;;
+end
+
+module Ast = struct
+
+  type t =
+    | Term      of string * t list
+    | Var       of string
+    | Sequence  of t list (* TODO: list vs array? *)
+    | Primitive of primitive
+    ;;
+
+end
 
 type var =
   | Var of string
@@ -26,6 +59,8 @@ type literal =
   | LitText of string
   | LitInteger of Bigint.t
 ;;
+
+module Core = struct
 
 type ty = | Ty;; (* TODO *)
 
@@ -51,6 +86,8 @@ and core =
   | Metavar of string
 ;;
 
+end
+
 type denotation_scope_pat =
   | DenotationScopePat of string list * denotation_pat
 
@@ -60,12 +97,40 @@ and denotation_pat =
 ;;
 
 type denotation_chart =
-  | DenotationChart of (denotation_pat * core) list
+  | DenotationChart of (denotation_pat * Core.core) list
 ;;
 
 (* val eval_core : core -> (core_val, string) result;; *)
-let eval_core (core : core) : (core_val, string) Result.t = match core with
-  | CoreVar v -> Error "TODO";;
+let eval_core (core : Core.core) : (Core.core_val, string) Result.t =
+
+  let open Belt.Result in
+  let open Core in
+
+  let rec go ctx core = match core with
+        | CoreVar (Var v) -> (match Belt.Map.String.get ctx v with
+          | Some result -> Ok result
+          | None        -> Error ("Unbound variable " ^ v))
+        | CoreVal v -> Ok v
+        | CoreApp (Lam (argNames, body), args) ->
+            if List.length argNames != List.length args
+            then Error "mismatched application lengths"
+            else let args' = [] (* List.map (go ctx) args *) in
+                 let ctx' = Belt.Map.String.merge
+                       ctx
+                       (Belt.Map.String.fromArray (List.toArray
+                         (List.zip argNames args')))
+                       (fun _k v1 v2 -> match (v1, v2) with
+                         | (_,      Some v) -> Some v
+                         | (Some v, None  ) -> Some v
+                         | (None,   None  ) -> None)
+                 in go ctx' body
+        (* | Case tm _ty branches ->
+          let v = go ctx tm *)
+        | Metavar _v -> Error "Found a metavar!"
+
+        | _ -> Error "TODO"
+
+  in go Belt.Map.String.empty core
 
 let rec intersperse_after list el =
   match list with
