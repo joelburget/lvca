94d1d75c7bf7a23d1b3bcbc994d4b30654b62478
Joel Burget <joelburget@gmail.com>
2019-06-28 07:52:19 -0700

    Use correctly cased name in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: {
-    index: './lib/js/src/index.bs.js'
+    index: './lib/js/src/Index.bs.js'
   },
   mode: isProd ? 'production' : 'development',
   output: {
