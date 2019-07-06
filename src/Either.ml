ffd1fe759761385c1c10840fe3cac67fec146ad2
Joel Burget <joelburget@gmail.com>
2019-07-06 10:34:49 -0700

    Add `Either` module.

---
+++ src/Either.ml
@@ -0,0 +1,11 @@
+type ('a, 'b) either =
+  | Left  of 'a
+  | Right of 'b
+
+let map f = function
+  | Left  a -> Left a
+  | Right b -> Right (f b)
+
+let (>>=) e f = match e with
+  | Left  a -> Left a
+  | Right b -> f b
