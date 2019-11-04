24a6b5141c5f0dd9d1b4db25bd3d8e587f1f0664
Joel Burget <joelburget@gmail.com>
2019-11-03 20:41:19 -0800

    Add start of an integer language and tests.

---
+++ __tests__/Languages_test.ml
@@ -0,0 +1,49 @@
+open Jest
+open Expect
+module Parseable_abstract_syntax' = ParseStatus.Make(Parsing.Parseable_abstract_syntax)
+module Parseable_concrete = ParseStatus.Make(Parsing.Parseable_concrete_syntax)
+module Parseable_term' = ParseStatus.Make(Parsing.Parseable_term)
+
+let () = describe "Integer Language" (fun () ->
+  test "parse abstract syntax" (fun () ->
+    let (_, language) =
+      Parseable_abstract_syntax'.parse LanguageInteger.abstractSyntax
+    in
+    match language with
+      | Belt.Result.Ok _ -> pass
+      | Error msg -> fail msg
+  );
+
+  (*
+  test "parse concrete syntax" (fun () ->
+    let (_, concrete) =
+      Parseable_concrete.parse LanguageInteger.concrete
+    in
+    match concrete with
+      | Belt.Result.Ok _ -> pass
+      | Error msg -> fail msg
+  );
+  *)
+
+  let eval_str str =
+    let _, tm = Parseable_term'.parse str in
+    match tm with
+      | Ok tm' -> LanguageInteger.eval_tm tm'
+      | Error err -> Error err
+  in
+
+  let mk_result_int i = Belt.Result.Ok
+    (Binding.Nominal.Primitive (PrimInteger (Bigint.of_int i)))
+  in
+
+  testAll "evaluate" [
+    expect (eval_str "add(1; 2)") |> toEqual (mk_result_int 3);
+    expect (eval_str "sub(1; 2)") |> toEqual (mk_result_int (-1));
+    expect (eval_str "abs(101)")  |> toEqual (mk_result_int 101);
+    expect (eval_str "abs(-101)") |> toEqual (mk_result_int 101);
+    expect (eval_str "neg(101)")  |> toEqual (mk_result_int (-101));
+    expect (eval_str "neg(-101)") |> toEqual (mk_result_int 101);
+    expect (eval_str "min(1; 2)") |> toEqual (mk_result_int 1);
+    expect (eval_str "max(1; 2)") |> toEqual (mk_result_int 2);
+  ] Util.id;
+)
---
+++ src/LanguageInteger.ml
@@ -0,0 +1,66 @@
+let abstractSyntax = {|
+tm :=
+  | neg(tm)
+  | abs(tm)
+  | add(tm; tm)
+  | sub(tm; tm)
+  | max(tm; tm)
+  | min(tm; tm)
+  // TODO: how to express literal
+|}
+
+let concrete = {|
+  // TODO
+|}
+
+(* TODO: lessen duplication *)
+let rec eval' : Bigint.t list -> Binding.DeBruijn.term -> Bigint.t option
+  = fun env tm -> match tm with
+    | Operator ("neg", [Scope([], a)])
+    -> (match eval' env a with
+      | Some a' -> Some (Bigint.neg a')
+      | _ -> None
+    )
+    | Operator ("abs", [Scope([], a)])
+    -> (match eval' env a with
+      | Some a' -> Some (Bigint.abs a')
+      | _ -> None
+    )
+    | Operator ("add", [Scope([], a); Scope([], b)])
+    -> (match eval' env a, eval' env b with
+      | Some a', Some b' -> Some (Bigint.add a' b')
+      | _, _ -> None
+    )
+    | Operator ("sub", [Scope([], a); Scope([], b)])
+    -> (match eval' env a, eval' env b with
+      | Some a', Some b' -> Some (Bigint.sub a' b')
+      | _, _ -> None
+    )
+    | Operator ("max", [Scope([], a); Scope([], b)])
+    -> (match eval' env a, eval' env b with
+      | Some a', Some b' -> Some (Bigint.max a' b')
+      | _, _ -> None
+    )
+    | Operator ("min", [Scope([], a); Scope([], b)])
+    -> (match eval' env a, eval' env b with
+      | Some a', Some b' -> Some (Bigint.min a' b')
+      | _, _ -> None
+    )
+    | Var i -> Belt.List.get env i
+    | Primitive (PrimInteger i) -> Some i
+    | Operator _
+    | Sequence _
+    | Primitive (PrimString _) -> None
+
+let eval : Binding.DeBruijn.term -> Bigint.t option
+  = eval' []
+
+let eval_tm
+  : Binding.Nominal.term -> (Binding.Nominal.term, string) Belt.Result.t
+  = fun tm ->
+    match Binding.DeBruijn.from_nominal tm with
+      | Error msg -> Error msg
+      | Ok db_tm -> (match eval db_tm with
+        | None -> Error "failed to evaluate"
+        | Some result -> Ok (Binding.Nominal.Primitive (PrimInteger result))
+      )
