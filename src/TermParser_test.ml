8a67827ddce1d5867e2aae1d2acde55a3aa0df06
Joel Burget <joelburget@gmail.com>
2020-03-15 18:05:52 -0700

    Add term parser test of patterns.

--- src/TermParser_test.ml
+++ src/TermParser_test.ml
@@ -8,13 +8,32 @@ let expect_parse str tm =
 
 let%test_module "TermParser" =
   (module struct
-    let%test "" = expect_parse "[x]" (Sequence [ Var "x" ])
-    let%test "" = expect_parse "x" (Var "x")
-    let%test "" = expect_parse "123" (Primitive (PrimInteger (Bigint.of_int 123)))
-    let%test "" = expect_parse "\"abc\"" (Primitive (PrimString "abc"))
+    let%test _ = expect_parse "[x]" (Sequence [ Var "x" ])
+    let%test _ = expect_parse "x" (Var "x")
+    let%test _ = expect_parse "123" (Primitive (PrimInteger (Bigint.of_int 123)))
+    let%test _ = expect_parse "\"abc\"" (Primitive (PrimString "abc"))
 
-    let%test "" =
+    let%test _ =
       expect_parse "lam(x. x)" (Operator ("lam", [ Scope ([ Var "x" ], Var "x") ]))
-    ;;
+
+    let%test _ =
+      let match_line a b = Operator ("match_line", [ Scope ([a], b) ]) in
+
+      expect_parse {|
+        match(x; [
+          match_line(foo(). true()),
+          match_line(bar(_; _x; y). y)
+        ])
+      |}
+      (Operator ("match", [
+        Scope ([], Var "x");
+        Scope ([], Sequence [
+          match_line (Pattern.Operator ("foo", [])) (Operator ("true", []));
+          match_line
+            (Pattern.Operator ("bar", [Ignored ""; Ignored "x"; Var "y"]))
+            (Var "y");
+        ]);
+      ]))
+
   end)
 ;;
