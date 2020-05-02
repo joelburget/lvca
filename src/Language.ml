f3806884778db63c02d7c5876455b8a22f524d0c
Joel Burget <joelburget@gmail.com>
2020-05-01 19:00:53 -0700

    Add Language functor to automate boilerplate.

---
+++ src/Language.ml
@@ -0,0 +1,84 @@
+open Core_kernel
+
+module type Description = sig
+  val abstract_syntax : string
+  val concrete_syntax : string
+  val statics : string
+  val expr_name : string
+end
+
+exception InferenceError
+
+module type S = sig
+  val abstract_syntax : AbstractSyntax.t
+  val concrete_syntax : ConcreteSyntaxDescription.t
+  val statics : Statics.rule list
+  val check : Statics.typing -> unit
+  val infer : Binding.Nominal.term -> Binding.Nominal.term
+  val parse_concrete : string
+    -> ( ConcreteSyntax.formatted_tree
+       , (LexerUtil.lex_error, LrParsing.parse_error) Core_kernel.Either.t
+       ) Core_kernel.Result.t
+  val to_ast
+    :  ConcreteSyntax.formatted_tree
+    -> (Binding.Nominal.term, string) Core_kernel.Result.t
+  val of_ast : Binding.Nominal.term -> ConcreteSyntax.formatted_tree
+end
+
+module Make(X : Description) : S = struct
+  let abstract_syntax : AbstractSyntax.t =
+    match Parsing.AbstractSyntax.parse X.abstract_syntax with
+      | Ok tm -> tm
+      | Error err -> failwith (ParseError.to_string err)
+  ;;
+
+  let concrete_syntax =
+    let pre_terminal_rules, sort_rules =
+      match Parsing.ConcreteSyntax.parse X.concrete_syntax with
+        | Ok tm -> tm
+        | Error err -> failwith (ParseError.to_string err)
+    in
+    ConcreteSyntax.make_concrete_description pre_terminal_rules sort_rules
+  ;;
+
+  let statics = match Parsing.Statics.parse X.statics with
+    | Ok statics -> statics
+    | Error err -> failwith (ParseError.to_string err)
+  ;;
+
+  let bidirectional_env = Bidirectional.
+    { rules = statics
+    ; var_types = String.Map.empty
+    }
+
+  let check : Statics.typing -> unit
+    = Bidirectional.check bidirectional_env
+  ;;
+
+  (** Infer the type of a term or raise [InferenceError]
+   @raise [FreeVar]
+   @raise [InferenceError]
+   *)
+  let infer : Binding.Nominal.term -> Binding.Nominal.term
+    = fun tm -> tm
+      |> Binding.DeBruijn.from_nominal
+      |> Result.ok_or_failwith
+      |> Statics.Types.of_de_bruijn
+      |> Bidirectional.infer bidirectional_env
+      |> Statics.Types.to_de_bruijn_exn
+      |> Binding.DeBruijn.to_nominal
+      |> Util.get_option InferenceError
+  ;;
+
+  let parse_concrete = ConcreteSyntax.parse concrete_syntax X.expr_name
+  ;;
+
+  let to_ast = ConcreteSyntax.to_ast concrete_syntax
+  ;;
+
+  let expr_sort = AbstractSyntax.Types.SortAp (X.expr_name, [||])
+
+  let of_ast = ConcreteSyntax.of_ast abstract_syntax.sort_defs
+    concrete_syntax expr_sort X.expr_name 80
+  ;;
+end
--- src/LanguageHuttonsRazor.ml
+++ src/LanguageHuttonsRazor.ml
@@ -1,126 +1,61 @@
 open Core_kernel
 
-let abstract_syntax_str =
-  {|
-import {integer} from "builtin:integer"
+module Description = struct
 
-expr :=
-  | lit(integer())    // an expression can be a literal integer
-  | add(expr(); expr()) // or the addition of two expressions
+  let abstract_syntax =
+    {|
+  import {integer} from "builtin"
 
-type := int() // there's only one type in the language
-  |}
-;;
+  expr :=
+    | lit(integer())    // an expression can be a literal integer
+    | add(expr(); expr()) // or the addition of two expressions
 
-let abstract : AbstractSyntax.t =
-  match Parsing.AbstractSyntax.parse abstract_syntax_str with
-    | Ok tm -> tm
-    | Error err -> failwith (ParseError.to_string err)
-;;
+  type := int() // there's only one type in the language
+    |}
+  ;;
 
-let concrete_syntax_str =
-  {|
-// terminals:
-ADD := "+"
-LPAREN := "("
-RPAREN := ")"
-INTEGER := /\d+/
-INT := "int"
+  let concrete_syntax =
+    {|
+  // terminals:
+  ADD := "+"
+  LPAREN := "("
+  RPAREN := ")"
+  INTEGER := /\d+/
+  INT := "int"
 
-// nonterminals:
+  // nonterminals:
 
-expr := x = expr_1 { x }
+  expr := x = expr_1 { x }
 
-// expressions at precedence level 1
-expr_1 :=
-  | x = expr_2 ADD y = expr_1 { add(x; y) }
-  | x = expr_2                { x         }
+  // expressions at precedence level 1
+  expr_1 :=
+    | x = expr_2 ADD y = expr_1 { add(x; y) }
+    | x = expr_2                { x         }
 
-// expressions at precedence level 2
-expr_2 :=
-  | x = INTEGER              { lit(integer(x)) }
-  | LPAREN x = expr_1 RPAREN { x               }
+  // expressions at precedence level 2
+  expr_2 :=
+    | x = INTEGER              { lit(integer(x)) }
+    | LPAREN x = expr_1 RPAREN { x               }
 
-type := INT { int() }
-  |}
-;;
+  type := INT { int() }
+    |}
+  ;;
 
-let concrete =
-  let pre_terminal_rules, sort_rules =
-    match Parsing.ConcreteSyntax.parse concrete_syntax_str with
-      | Ok tm -> tm
-      | Error err -> failwith (ParseError.to_string err)
-  in
-  ConcreteSyntax.make_concrete_description pre_terminal_rules sort_rules
-;;
+  let statics =
+    {|
+  --------------------------
+  ctx >> lit(_) => int()
 
-let statics_str =
-  {|
---------------------------
-ctx >> lit(_) => int()
+  -----------------------------
+  ctx >> add(_; _) => int()
+    |}
+  ;;
 
------------------------------
-ctx >> add(_; _) => int()
-  |}
-;;
+  let expr_name = "expr";;
 
-let statics = match Parsing.Statics.parse statics_str with
-  | Ok statics -> statics
-  | Error err -> failwith (ParseError.to_string err)
-;;
+end
 
-let bidirectional_env = Bidirectional.
-  { rules = statics
-  ; var_types = String.Map.empty
-  }
-
-let check : Statics.typing -> unit
-  = Bidirectional.check bidirectional_env
-;;
-
-exception InferenceError
-
-(**
- @raise [FreeVar]
- @raise [InferenceError]
- *)
-let infer : Binding.Nominal.term -> Binding.Nominal.term
-  = fun tm -> tm
-    |> Binding.DeBruijn.from_nominal
-    |> Result.ok_or_failwith
-    |> Statics.Types.of_de_bruijn
-    |> Bidirectional.infer bidirectional_env
-    |> Statics.Types.to_de_bruijn_exn
-    |> Binding.DeBruijn.to_nominal
-    |> Util.get_option InferenceError
-;;
-
-(*
-let dynamics_str =
-  {|
-dynamics = \(expr : expr()) -> match expr with {
-  | add(a; b) -> #add(dynamics a; dynamics b)
-  | lit(i) -> i
-}
-  |}
-;;
-
-let dynamics = Parsing.Dynamics.parse dynamics_str |> Result.ok_or_failwith
-;;
-*)
-
-(* TODO: write a functor to do this stuff for any language? *)
-
-let parse_concrete = ConcreteSyntax.parse concrete "expr"
-;;
-
-let to_ast = ConcreteSyntax.to_ast concrete
-;;
-
-let expr_sort = AbstractSyntax.Types.SortAp ("expr", [||])
-
-let of_ast = ConcreteSyntax.of_ast abstract.sort_defs concrete expr_sort "expr" 80
-;;
+open Language.Make(Description)
 
 let%expect_test {|pretty lit(1)|} =
   let tm = Binding.Nominal.(Operator ("lit",
@@ -129,3 +64,4 @@ let%expect_test {|pretty lit(1)|} =
   in
   print_string (ConcreteSyntax.to_string (of_ast tm));
   [%expect{| 1 |}]
+
