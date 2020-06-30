b6d24f2ffd11853312d8a9ab81c67cf5cbc6adbe
Joel Burget <joelburget@gmail.com>
2020-06-29 17:48:16 -0700

    WIP parser revamp.

--- languages/LanguageParser.ml
+++ languages/LanguageParser.ml
@@ -3,7 +3,7 @@ open Lvca
 module Format = Caml.Format
 
 let abstract_syntax_str = {|
-import { char } from "lvca/builtin"
+import { char, string } from "lvca/builtin"
 import { term as n_term } from "lvca/term"
 import { term as c_term } from "lvca/core"
 
@@ -11,15 +11,16 @@ parsers := parsers(parser()*)
 
 parser :=
   // primitive parsers
-  | char(c_term())
-  | string(c_term())
+  | char(char())
+  | string(string())
   | satisfy(char(). c_term())
-  | named(c_term(); parser())
   | fail(c_term())
+
+  | let(parser(); parser(). parser())
 
   // combinators
   | option(parser())
-  | count(c_term(); parser())
+  | count(parser(); c_term())
   | many(parser())
   | many1(parser())
   | fix(parser(). parser())
@@ -48,15 +49,16 @@ type n_term = Binding.Nominal.term
 
 type t =
   (* primitive parsers *)
-  | Char of c_term
-  | String of c_term
+  | Char of char
+  | String of string
   | Satisfy of string * c_term
-  | Named of c_term * t
   | Fail of c_term
+
+  | Let of string * t * t
 
   (* combinators *)
   | Option of t
-  | Count of c_term * t
+  | Count of t * c_term
   | Many of t
   | Many1 of t
   | Fix of string * t
@@ -74,100 +76,101 @@ type t =
 
 let rec pp : t Fmt.t (* Format.formatter -> t -> unit *)
   = fun ppf ->
-    let term = Core.pp in
+    let core = Core.pp in
     let pf = Fmt.pf in
     function
-    | Char tm -> pf ppf "char %a" term tm
-    | String tm -> pf ppf "string %a" term tm
-    | Satisfy (name, tm) -> pf ppf "satisfy (\\%s. %a)" name term tm
-    | Named (tm, t) -> pf ppf "named %a %a" term tm pp t
-    | Fail tm -> pf ppf "fail %a" term tm
-    | Option t -> pf ppf "option %a" pp t
-    | Count (t, p) -> pf ppf "count %a %a" term t pp p
-    | Many t -> pf ppf "many %a" pp t
-    | Many1 t -> pf ppf "many1 %a" pp t
+    | Char char -> pf ppf "'%c'" char
+    | String str -> pf ppf {|"%s"|} str
+    | Satisfy (name, tm) -> pf ppf "satisfy (\\%s. %a)" name core tm
+    | Let (name, named, body) -> pf ppf "let %s = %a in %a" name pp named pp body
+    | Fail tm -> pf ppf "fail %a" core tm
+    | Option t -> pf ppf "%a?" pp t
+    | Count (p, t) -> pf ppf "%a{%a}" pp p core t
+    | Many t -> pf ppf "%a*" pp t
+    | Many1 t -> pf ppf "%a+" pp t
     | Fix (name, t) -> pf ppf "fix (%s -> %a)" name pp t
     | Alt (t1, t2) -> pf ppf "alt %a %a" pp t1 pp t2
-    | Return tm -> pf ppf "return %a" term tm
+    | Return tm -> pf ppf "return %a" core tm
     | Left (t1, t2) -> pf ppf "%a <* %a" pp t1 pp t2
     | Right (t1, t2) -> pf ppf "%a *> %a" pp t1 pp t2
     | LiftN (names, p, ps) -> pf ppf "lift (\\%a. %a) [%a]"
       Fmt.(list ~sep:(any ".@ ") string) names
-      term p
+      core p
       Fmt.(list ~sep:comma pp) ps
 
 module Parse(Comment : Util.Angstrom.Comment_int) = struct
   module Parsers = Util.Angstrom.Mk(Comment)
 
-  let char_lit, parens, string = Parsers.(char_lit, parens, string)
-  let choice, fix, lift3, many1, return, ( *> ), (<$>), (<?>), (>>=) =
-    Angstrom.(choice, fix, lift3, many1, return, ( *> ), (<$>), (<?>), (>>=))
+  let char, char_lit, string_lit, parens, string =
+    Parsers.(char, char_lit, string_lit, parens, string)
+  let choice, fix, lift2, lift3, many1, return, ( <* ), ( *> ), (<$>), (<?>), (>>=), (>>|) =
+    Angstrom.(choice, fix, lift2, lift3, many1, return, ( <* ), ( *> ), (<$>), (<?>), (>>=), (>>|))
 
   (* Note: "many1" must occur before "many", etc.
    * Eventually I would like to move to a representation without this limitation.
    *)
   let keywords : string list =
-    [ "char"; "string"; "satisfy"; "option"; "many1"; "many"; "alt";
-      "named"; "fail"; "return" ]
+    [ "satisfy"; (* "option"; "many1"; "many"; "alt"; *)
+      "let"; "in"; "fail"; (* "return" *) ]
 
   let keyword : string Angstrom.t
     = keywords |> List.map ~f:string |> choice
 
-  let operators : string list = [ "<*"; "*>" ]
+  let operators : string list = [ "?"; "*"; "+"; "|"; "=" ]
 
   let operator : string Angstrom.t
     = operators |> List.map ~f:string |> choice
 
+  (*
   type atom =
+    | CharLit of char
+    | StringLit of string
+
     | Keyword of string
     | Name of string
     | Operator of string
     | Parser of t
     | Term of c_term
 
-  let pp_atom : atom Fmt.t (* Format.formatter -> atom -> unit*)
+  let pp_atom : atom Fmt.t (* = Format.formatter -> atom -> unit*)
     = fun ppf ->
       let pf = Fmt.pf in
       function
-      | Keyword str -> pf ppf "%s" str
-      | Name str -> pf ppf "%s" str
-      | Operator str -> pf ppf "%s" str
-      | Parser p -> pf ppf "(%a)" pp p
-      | Term tm -> Core.pp ppf tm
+        | CharLit c -> pf ppf "'%c'" c
+        | StringLit s -> pf ppf {|"%s"|} s
+        | Keyword str -> pf ppf "%s" str
+        | Name str -> pf ppf "%s" str
+        | Operator str -> pf ppf "%s" str
+        | Parser p -> pf ppf "(%a)" pp p
+        | Term tm -> Core.pp ppf tm
+        *)
+
+  let parse_in_precedence : int -> c_term Angstrom.t -> t Angstrom.t
+    = fun _prec term -> fix (fun parser -> choice
+      [ (fun c -> Char c) <$> char_lit
+      ; (fun s -> String s) <$> string_lit
+      ; string "satisfy" *>
+        (parens
+          (lift3
+            (fun name _arr tm -> Satisfy (name, tm))
+            Parsers.identifier
+            (string "->")
+            term))
+      ; lift3
+        (fun name bound body -> Let (name, bound, body))
+        (string "let" *> Parsers.identifier)
+        (string "=" *> parser)
+        (string "in" *> parser)
+      (* XXX: add identifiers *)
+      ; string "fail" *> term >>| (fun tm -> Fail tm)
+      ; parser <* char '?' >>| (fun p -> Option p)
+      ; parser <* char '*' >>| (fun p -> Many p)
+      ; parser <* char '+' >>| (fun p -> Many1 p)
+      ]
+    ) <?> "parser"
 
   let t : c_term Angstrom.t -> t Angstrom.t
-    = fun term -> fix (fun parser ->
-
-      let atomic_t = choice
-        [ (fun k -> Keyword k) <$> keyword
-        ; (fun n -> Name n) <$> Parsers.identifier
-        ; (fun o -> Operator o) <$> operator
-        ; (fun p -> Parser p) <$> parens parser
-        ; (fun t -> Term t) <$> term
-        ]
-      in
-
-      many1 atomic_t >>= fun atoms -> match atoms with
-        | [ Keyword "char"; Term tm ] -> return (Char tm)
-        | [ Keyword "string"; Term tm ] -> return (String tm)
-        (* TODO: punctuation *)
-        | [ Keyword "satisfy"; Name name; Term tm ] -> return (Satisfy (name, tm))
-        | [ Keyword "named"; Term tm; Parser p ] -> return (Named (tm, p))
-        | [ Keyword "fail"; Term tm ] -> return (Fail tm)
-        | [ Keyword "option"; Parser p ] -> return (Option p)
-        (* TODO: count *)
-        | [ Keyword "many"; Parser p ] -> return (Many p)
-        | [ Keyword "many1"; Parser p ] -> return (Many1 p)
-        (* TODO: fix *)
-        | [ Keyword "alt"; Parser p1; Parser p2 ] -> return (Alt (p1, p2))
-        (* TODO: choice *)
-        | [ Keyword "return"; Term tm ] -> return (Return tm)
-        | [ Parser p1; Operator "<*"; Parser p2 ] -> return (Left (p1, p2))
-        | [ Parser p1; Operator "*>"; Parser p2 ] -> return (Right (p1, p2))
-        (* TODO: liftn *)
-        | _ -> Angstrom.fail (Fmt.str "%a" (Fmt.list pp_atom ~sep:Fmt.sp) atoms)
-
-    ) <?> "parser"
+    = parse_in_precedence 0
 end;;
 
 let scope_list : n_term list -> Binding.Nominal.scope list
@@ -179,45 +182,48 @@ let mk_list : n_term list -> n_term
 let mk_some : n_term -> n_term
   = fun tm -> Binding.Nominal.Operator ("some", [Scope ([], tm)])
 
+type ctx_entry =
+  | BoundChar of char
+  | BoundParser of t
+
+(* TODO: this is hacky *)
+let thin_ctx : ctx_entry Util.String.Map.t -> n_term Util.String.Map.t
+  = Map.filter_map ~f:(function
+    | BoundChar c -> Some (Binding.Nominal.Primitive (PrimChar c))
+    | BoundParser _ -> None
+  )
+
 (* Translate our parser type into an angstrom parser *)
 let translate : t -> n_term Angstrom.t
   = let open Angstrom in
     let mk_err () = failwith "TODO: error" in
 
     let rec translate' ctx = function
-    | Char tm ->
-      let c = match Core.eval_ctx_exn ctx tm with
-        | Primitive (PrimChar c) -> c
-        | _ -> mk_err ()
-      in
+    | Char c ->
       char c >>| (fun c -> Binding.Nominal.Primitive (PrimChar c))
         <?> (Printf.sprintf {|char '%s'|} (String.make 1 c))
-    | String tm ->
-      let str = match Core.eval_ctx_exn ctx tm with
-        | Primitive (PrimString str) -> str
-        | _ -> mk_err ()
-      in
+    | String str ->
       string str >>| (fun str -> Binding.Nominal.Primitive (PrimString str))
         <?> (Printf.sprintf {|string "%s"|} str)
     | Satisfy (name, tm) ->
       let f c =
-        let ctx' = Map.set ctx ~key:name ~data:(Primitive (PrimChar c)) in
-        match Core.eval_ctx_exn ctx' tm with
+        let ctx' = Map.set ctx ~key:name ~data:(BoundChar c) in
+        match Core.eval_ctx_exn (thin_ctx ctx') tm with
           | Operator ("true", []) -> true
           | _ -> false
       in
       satisfy f >>| fun c -> Binding.Nominal.Primitive (PrimChar c)
-    | Named (tm, p) -> (match Core.eval_ctx_exn ctx tm with
-      | Primitive (PrimString name) -> translate' ctx p <?> name
-      | _ -> mk_err ())
-    | Fail tm -> (match Core.eval_ctx_exn ctx tm with
+    | Let (name, named, body) ->
+      let ctx' = Map.set ctx ~key:name ~data:(BoundParser named) in
+      translate' ctx' body <?> name
+    | Fail tm -> (match Core.eval_ctx_exn (thin_ctx ctx) tm with
       | Primitive (PrimString msg) -> fail msg
       | _ -> mk_err ())
     | Option p -> option None ((fun tm -> Some tm) <$> translate' ctx p) >>| (function
       | Some tm -> mk_some tm
       | None -> Operator ("none", []))
-    | Count (n_tm, p) ->
-      let n = match Core.eval_ctx_exn ctx n_tm with
+    | Count (p, n_tm) ->
+      let n = match Core.eval_ctx_exn (thin_ctx ctx) n_tm with
         | Primitive (PrimInteger i) -> (match Bigint.to_int i with
           | Some n -> n
           | None -> mk_err ()
@@ -263,33 +269,51 @@ let%test_module "Parsing" = (module struct
           | Ok tm -> Binding.Nominal.pp_term Caml.Format.std_formatter tm)
 
   let%expect_test _ =
-    parse' {|string {"str"}|} "str";
+    parse' {|"str"|} "str";
     [%expect{| "str" |}]
 
   let%expect_test _ =
-    parse' {|string {"str"}|} "foo";
+    parse' {|"str"|} "foo";
     [%expect{| failed to parse: string "str": string |}]
 
   let%expect_test _ =
-    parse' {|many1 (string {"str"})|} "strstrstr";
+    parse' {|"str"*|} "strstrstr";
     [%expect{| list("str"; "str"; "str") |}]
 
   let%expect_test _ =
+    parse' {|"str"+|} "strstrstr";
+    [%expect{| list("str"; "str"; "str") |}]
+
+    (*
+  let%expect_test _ =
     (* TODO: proper precedence: remove parens *)
-    parse' {|(string {"str"}) *> (string {"foo"})|} "strfoo";
+    parse' {|"str" *> "foo")|} "strfoo";
     [%expect{| "foo" |}]
 
   let%expect_test _ =
     (* TODO: proper precedence: remove parens *)
-    parse' {|(string {"str"}) <* (string {"foo"})|} "strfoo";
+    parse' {|"str" <* "foo"|} "strfoo";
     [%expect{| "str" |}]
+    *)
 
     (* TODO: proper syntax for binding *)
-  let sat_parser = {|satisfy x (match x with {
+  let sat_parser = {|satisfy (x -> match x with {
       | 'c' -> {true()}
       | _ -> {false()}
     })
     |}
+
   let%expect_test _ = parse' sat_parser "c"; [%expect{| 'c' |}]
   let%expect_test _ = parse' sat_parser "d"; [%expect{| failed to parse: : satisfy: 'd' |}]
+
+  (*
+  let%expect_test _ =
+    parse' {|let x = "str" in x|} "str";
+    [%expect{| "str" |}]
+    *)
+
+  let%expect_test _ =
+    parse' {|fail {"reason"}|} "str";
+    (* TODO: nicer formatting *)
+    [%expect{| failed to parse: : reason |}]
 end);;
