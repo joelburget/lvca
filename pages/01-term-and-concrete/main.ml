bbe2df0a4e5b3a8be806f7df516f13e6f5d94e17
Joel Burget <joelburget@gmail.com>
2020-08-23 14:13:25 -0700

    Checkpoint 01-term-and-concrete.

--- pages/01-term-and-concrete/main.ml
+++ pages/01-term-and-concrete/main.ml
@@ -32,41 +32,39 @@ type update_fun = ?step:React.step -> Model.t -> unit
 module Action = struct
   type t =
     | Evaluate of string
+    | Unselect
     | Select of int * int
     | SwitchInputLang
 end
+
+let parser_of = function
+  | Lambda -> LambdaParse.t
+  | Term -> TermParse.t
 
 module Controller = struct
   let update (action : Action.t) model_s signal_update =
     let open Model in
 
-    let { input; result; input_lang; selected = _ } = React.S.value model_s in
+    let { input; result; input_lang; selected } = React.S.value model_s in
     let new_model = match action with
       | Action.Evaluate str ->
-        begin
-          let parser = match input_lang with
-            | Lambda -> LambdaParse.t
-            | Term -> TermParse.t
-          in
-          let result = parse parser str in
-          let selected = None in
-          { input; result; input_lang; selected }
-        end
+        { input; input_lang; result = parse (parser_of input_lang) str; selected }
+      | Unselect -> { input; result; input_lang; selected = None }
       | Select (start, finish) ->
-        let selected = Some Range.{ start; finish } in
-        { input; result; input_lang; selected }
+        { input; result; input_lang; selected = Some Range.{ start; finish } }
       | SwitchInputLang ->
-        let input_lang' = match input_lang with
-          | Lambda -> Term
-          | Term -> Lambda
+        let input_lang', formatter = match input_lang with
+          | Lambda -> Term, term_pretty
+          | Term -> Lambda, lambda_pretty
         in
-        let input' = match result with
-          | Error _ -> ""
-          | Ok tm -> match input_lang with
-            | Lambda -> Fmt.str "%a" term_pretty tm
-            | Term -> Fmt.str "%a" lambda_pretty tm
+        let input', result' = match result with
+          | Error _ -> "", result
+          | Ok tm ->
+            (* TODO: clean up / explain *)
+            let result'_str = Fmt.str "%a" formatter tm in
+            result'_str, parse (parser_of input_lang') result'_str
         in
-        { input = input'; input_lang = input_lang'; selected = None; result }
+        { input = input'; input_lang = input_lang'; selected = None; result = result' }
     in
     signal_update new_model
 end
@@ -100,6 +98,7 @@ let mk_range_formatter
     | Some (rng, _) ->
         let style = rng_signal
           |> React.S.map (fun selected_rng ->
+            (* Highlight if this is a subset of the selected range *)
             if Range.(rng < selected_rng)
             then "background-color: rgba(33 150 243 / 50%);"
             else ""
@@ -130,10 +129,8 @@ let mk_range_formatter
     { mark_open_stag = (fun stag ->
       begin
         match stag with
-          | Range.Stag rng ->
-            (* Fmt.pr "Opening stag %a\n" Range.pp rng; *)
-            Stack.push stack (rng, Queue.create ())
-          | Caml.Format.String_tag _str -> (* Fmt.pr "Opening stag %s\n" str; *) ()
+          | Range.Stag rng -> Stack.push stack (rng, Queue.create ())
+          | Caml.Format.String_tag _str -> ()
           | _ -> ()
       end;
       ""
@@ -144,7 +141,7 @@ let mk_range_formatter
         | None ->
             begin
               match stag with
-                | Caml.Format.String_tag _str -> (* Fmt.pr "Closing stag %s\n" str; *) ()
+                | Caml.Format.String_tag _str -> ()
                 | _ -> ()
             end;
         | Some (_, q) ->
@@ -191,27 +188,27 @@ module View = struct
     Ev.async @@ (fun () -> ev elem handler)
 
   let mk_input model_s signal_update =
-    let input = model_s
-      |> React.S.map (fun m -> m.Model.input)
-      |> fun (value : string React.signal) -> R.Html5.(textarea
-        ~a:[
-          a_rows (React.S.const 25);
-          a_cols (React.S.const 60);
-          a_autofocus ();
-          a_class (React.S.const ["input"]);
-        ]
-        (React.S.const (txt value)))
+    let input = Html5.(textarea
+      ~a:[
+        a_rows 15;
+        a_cols 60;
+        a_autofocus ();
+        a_class ["input"];
+      ])
+      (model_s |> React.S.map (fun m -> m.Model.input) |> R.Html5.txt)
     in
     let input_dom = To_dom.of_textarea input in
 
     bind_event Ev.keydowns input_dom (fun evt ->
-      let key = evt##.key |> Js.Optdef.to_option |> Option.map ~f:Js.to_string in
-      Lwt.return (match key with
-        (* TODO: require special key *)
-        | Some "Enter"
-        -> Controller.update (Evaluate (Js.to_string input_dom##.value))
-          model_s signal_update
-        | _ -> ())
+      Lwt.return (
+        if WebUtil.is_special_enter evt
+        then
+          begin
+            Dom.preventDefault evt;
+            Controller.update (Evaluate (Js.to_string input_dom##.value))
+              model_s signal_update
+          end
+        else ())
       );
 
     bind_event Ev.selects input_dom (fun evt ->
@@ -231,6 +228,11 @@ module View = struct
       Lwt.return ()
       );
 
+    bind_event Ev.clicks input_dom (fun _evt ->
+      Controller.update Unselect model_s signal_update;
+      Lwt.return ()
+      );
+
     input
 
   let mk_output model_s =
@@ -240,7 +242,7 @@ module View = struct
         | None -> Range.mk 0 0)
     in
 
-    let code_s : [> `Code ] Html5.elt React.signal = model_s
+    let formatted_s : [> `Code ] Html5.elt React.signal = model_s
       |> React.S.map (fun Model.{ result; input_lang; _ } ->
         let elt, formatter = mk_range_formatter range_s in
         begin
@@ -257,7 +259,7 @@ module View = struct
 
     R.Html5.div
       ~a:[ R.Html5.a_class (React.S.const ["output"]) ]
-      (RList.singleton_s code_s)
+      (RList.singleton_s formatted_s)
 
   let info model_s =
     let msg_s = model_s
@@ -293,8 +295,7 @@ let main _ =
     Js.Opt.get (doc##getElementById (Js.string "app"))
       (fun () -> assert false)
   in
-  let m = Model.initial_model in
-  let model_s, signal_update = React.S.create m in
+  let model_s, signal_update = React.S.create Model.initial_model in
   Dom.appendChild parent
     (Js_of_ocaml_tyxml.Tyxml_js.To_dom.of_div (View.view model_s signal_update));
   Lwt.return ()
