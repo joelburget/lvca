f6cd4bf37fa3faa531e1fde558d007ba7ef87a04
Joel Burget <joelburget@gmail.com>
2020-09-17 17:04:46 -0700

    Add a few cleanups.

--- pages/Common.ml
+++ pages/Common.ml
@@ -4,12 +4,6 @@ open ReactiveData
 
 type term = (OptRange.t, Primitive.t) Nominal.term
 
-type lang =
-  | Lambda
-  | Term
-
-module PrimitiveParse = Primitive.Parse (ParseUtil.NoComment)
-module TermParse = Nominal.Parse (ParseUtil.NoComment)
 module LambdaParse = Lvca_languages.LambdaCalculus.AngstromParse (ParseUtil.NoComment)
 
 module Action = struct
@@ -19,9 +13,6 @@ module Action = struct
     | Select of int * int
     | SwitchInputLang
 end
-
-let parser_of = function Lambda -> LambdaParse.t | Term -> TermParse.t PrimitiveParse.t
-let term_pretty = Nominal.pp_term_range Primitive.pp (* XXX why used twice? *)
 
 let lambda_pretty = Lvca_languages.LambdaCalculus.pp (* XXX why used twice? *)
 
