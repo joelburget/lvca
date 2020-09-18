b23866d65d3fa42ee02ea455ed0e57f870de6eb6
Joel Burget <joelburget@gmail.com>
2020-09-17 19:14:16 -0700

    Get crowbar initially working.

--- crowbar/main.ml
+++ crowbar/main.ml
@@ -1,21 +1,23 @@
 open Lvca_syntax
 open Core_kernel
 
-(*
 (* The range vars are allowed to span. This is a list of de bruijn indices and
  * the number of vars bound at each index. *)
-type var_range = list int
+type var_range = int list
 
-let de_bruijn_gen : var_range -> Binding.DeBruijn.term gen
+(*
+let de_bruijn_gen : var_range -> (unit, Primitive.t) DeBruijn.term Crowbar.gen
   =
   (* let open Crowbar in *)
   Crowbar.fix (fun de_bruijn_gen ->
+    let open DeBruijn in
+    let open Primitive in
     let options =
-      [ Crowbar.(map [int] (fun i -> Primitive (PrimInteger (Bigint.of_int i))))
-      ; Crowbar.(map [string] (fun str -> Primitive (PrimString str)))
-      ; Crowbar.(map [list de_bruijn_gen] (fun tms -> Sequence tms))
+      [ Crowbar.(map [int] (fun i -> Primitive ((), PrimInteger (Bigint.of_int i))))
+      ; Crowbar.(map [bytes] (fun str -> Primitive ((), PrimString str)))
+      (* ; Crowbar.(map [list de_bruijn_gen] (fun tms -> Sequence tms)) *)
       (* TODO: use range *)
-      ; Crowbar.(map [pair
+      (* ; Crowbar.(map [pair *)
       ]
     in
     let options' = match var_range with
@@ -27,7 +29,6 @@ let de_bruijn_gen : var_range -> Binding.DeBruijn.term gen
         in var_gen :: options
     in
     Crowbar.choose options')
-*)
 
 module ParseNominal = Nominal.Parse (ParseUtil.NoComment)
 module ParsePrimitive = Primitive.Parse (ParseUtil.NoComment)
@@ -39,10 +40,12 @@ let term_str_conf tm_str =
      | Ok tm ->
          let tm_str' = Nominal.pp_term_str Primitive.pp tm in
          Crowbar.check_eq tm_str tm_str'
-
-(* TODO: term meta-model string -> term -> string | ^ ^ | | | | v | +----> term2 ----+ *)
+         *)
 
 let () =
   (* let _term_body = In_channel.read_all "term.lvca" in *)
-  Crowbar.(add_test ~name:"nominal round trip" [ bytes ] term_str_conf)
+  (* Crowbar.(add_test ~name:"Nominal round trip" [ bytes ] term_str_conf); *)
+  Crowbar.(add_test ~name:"Primitive string_round_trip1" [ bytes ]
+    (fun str -> str |> Primitive.Properties.string_round_trip2 |> check));
+  ()
 ;;
