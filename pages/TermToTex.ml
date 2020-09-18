96db5a6966760e9f6580860f126a51172b94f47c
Joel Burget <joelburget@gmail.com>
2020-09-17 17:51:41 -0700

    Work on term to tex demo.

--- pages/TermToTex.ml
+++ pages/TermToTex.ml
@@ -1,16 +1,10 @@
 open Base
-(* open Js_of_ocaml *)
-(* open Lvca_syntax *)
-(* open ReactiveData *)
 
 module Model = struct
   type t = string
 
   let initial_model = {|\overbrace{a+b+c}^{\text{note}}|}
 end
-
-type signal = Model.t React.signal
-type update_fun = ?step:React.step -> Model.t -> unit
 
 module Action = struct
   type t = Evaluate of string
@@ -30,32 +24,37 @@ module View = struct
   open Js_of_ocaml_tyxml.Tyxml_js
   module Ev = Js_of_ocaml_lwt.Lwt_js_events
 
-  let view _model_s _signal_update =
-    Html5.(
-      div
-        [ h2 [ txt "Term to TeX" ]
-        (*
-        ; div
-            ~a:[ a_class [ "container" ] ]
-            [ div
-                ~a:[ a_class [ "side" ] ]
-                [ h3 [ R.Html5.txt input_desc ]; mk_input model_s signal_update ]
-            ; div
-                ~a:[ a_class [ "switch-languages" ] ]
-                [ button
-                    ~a:
-                      [ a_onclick (fun _evt ->
-                            Controller.update SwitchInputLang model_s signal_update;
-                            false)
-                      ]
-                    [ txt "switch input languages" ]
-                ]
-            ; div
-                ~a:[ a_class [ "side" ] ]
-                [ h3 [ R.Html5.txt output_desc ]; mk_output model_s ]
-            ]
-        *)
-        ])
+  let view model_s signal_update =
+    let katex_area = Html5.div [] in
+    let katex_dom = To_dom.of_div katex_area in
+    let set_katex = Katex.render katex_dom in
+
+    let input, input_event = Common.mk_input model_s in
+
+    let _ : unit React.event = input_event |> React.E.map (function
+      | Common.InputUpdate str
+      ->
+        Caml.Printf.printf {|printing "%s"\n|} str;
+        set_katex str; (* XXX *)
+        Controller.update (Action.Evaluate str) model_s signal_update
+      | _ -> ()
+    )
+    in
+
+    [%html {|
+      <div>
+        <h2>Term to TeX</h2>
+        <div class="container">
+          <div class="side">
+            <h3>|}[ input ]{|</h3>
+          </div>
+          <div class="side">
+            <h3>output (rendered)</h3>
+            |}[ katex_area ]{|
+          </div>
+        </div>
+      </div>
+    |}]
   ;;
 end
 
