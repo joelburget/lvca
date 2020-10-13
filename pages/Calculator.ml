10c585bff45dbd7af411712b747924acf3d8d16d
Joel Burget <joelburget@gmail.com>
2020-10-12 18:38:43 -0700

    Add digits and platform-specific instructions to calculator.

--- pages/Calculator.ml
+++ pages/Calculator.ml
@@ -2,30 +2,87 @@ open Base
 open Lvca_syntax
 
 module Model = struct
-  type t = string
+  type t = string * int
 
-  let initial_model = {|1 + 1|}
+  let initial_model = {|1 + 1|}, 10
 end
 
 module Action = struct
-  type t = Evaluate of string
+  type digits_update =
+    | SetDigits of int
+    | IncrDigits
+    | DecrDigits
+
+  type t =
+    | Evaluate of string
+    | ChangePrecision of digits_update
 end
 
 module Controller = struct
-  let update (action : Action.t) _model_s signal_update =
+  let update (action : Action.t) model_s signal_update =
     (* let open Model in *)
-    let new_model = match action with Evaluate str -> str in
+    let str, digits = React.S.value model_s in
+    let new_model = match action with
+      | Evaluate str -> str, digits
+      | ChangePrecision (SetDigits digits)
+      -> str, digits
+      | ChangePrecision IncrDigits
+      -> str, digits + 1
+      | ChangePrecision DecrDigits
+      -> str, digits - 1
+    in
     signal_update new_model
   ;;
 end
 
 module View = struct
+  open Js_of_ocaml
   open Js_of_ocaml_tyxml.Tyxml_js
   module Ev = Js_of_ocaml_lwt.Lwt_js_events
   module Parse = Lvca_languages.Calculator.Parse (ParseUtil.CComment)
 
+  let mk_digits_entry digits_s =
+    let digits_event, signal_digits_event = React.E.create () in
+    let input = [%html{|<input type="text">|}] in
+    let input_dom = To_dom.of_input input in
+    Common.bind_event Ev.keydowns input_dom (fun evt ->
+      let key_name = evt##.code
+        |> Js.Optdef.to_option
+        |> Option.value_exn
+        |> Js.to_string
+      in
+      let result = match key_name with
+        | "Enter" -> (
+         Dom.preventDefault evt;
+         try
+           signal_digits_event
+             (Action.SetDigits (Int.of_string (Js.to_string input_dom##.value)))
+         with
+           _ -> ()
+        )
+        | "ArrowUp" | "ArrowRight" ->
+          Dom.preventDefault evt;
+          signal_digits_event Action.IncrDigits
+        | "ArrowDown" | "ArrowLeft" ->
+          Dom.preventDefault evt;
+          signal_digits_event Action.DecrDigits
+        | _ -> ()
+      in
+      Lwt.return result
+    );
+
+    let (_: unit React.event) = digits_s
+      (* Create an event when the input has changed *)
+      |> React.S.map ~eq:Caml.(=) Fn.id
+      |> React.S.changes
+      |> React.E.map (fun i -> input_dom##.value := Js.string (Int.to_string i))
+    in
+    input, digits_event
+
   let view model_s signal_update =
-    let input, input_event = Common.mk_input model_s in
+    let input, input_event = Common.mk_input (model_s |> React.S.map fst) in
+    let digits_entry, digits_event = mk_digits_entry (model_s |> React.S.map snd) in
+
     let (_ : unit React.event) = input_event
       |> React.E.map (function
         | Common.InputUpdate str ->
@@ -33,13 +90,27 @@ module View = struct
         | _ -> ())
     in
 
+    let (_ : unit React.event) = digits_event
+      |> React.E.map (fun update ->
+        Controller.update (ChangePrecision update) model_s signal_update)
+    in
+
     let result = model_s
-      |> React.S.map (fun str -> match ParseUtil.parse_string Parse.t str with
+      |> React.S.map (fun (str, digits) -> match ParseUtil.parse_string Parse.t str with
         | Error msg -> msg
         | Ok tm -> match Lvca_languages.Calculator.interpret tm with
           | Error (_tm, msg) -> msg
-          | Ok real -> Constructive_real.ConstructiveReal.eval_to_string real
+          | Ok real ->
+            let digits = Int32.of_int_exn digits in
+            Constructive_real.ConstructiveReal.eval_to_string real ~digits
       )
+    in
+
+    let eval_button = Html.(button [txt "evaluate"]) in
+
+    let instructions = match WebUtil.platform_special_combo () with
+      | None -> Html.txt ""
+      | Some instructions -> Html.(span ([ txt "by pressing " ] @ instructions))
     in
 
     [%html {|
@@ -53,6 +124,10 @@ module View = struct
             <h3>result</h3>
             |}[ R.Html.txt result ]{|
           </div>
+        </div>
+        <div>
+          <p>|}[eval_button; instructions]{|</p>
+          <p>digits: |}[digits_entry]{|</p>
         </div>
       </div>
     |}]
--- pages/WebUtil.ml
+++ pages/WebUtil.ml
@@ -1,6 +1,39 @@
 open Base
 open Js_of_ocaml
 
+type platform =
+  | Mac
+  | IOs
+  | Windows
+  | Android
+  | Linux
+  | Unknown
+
+let get_platform () =
+  let user_agent = Dom_html.window##.navigator##.userAgent |> Js.to_string in
+  let platform = Dom_html.window##.navigator##.platform |> Js.to_string in
+
+  let mac_re = Regexp.regexp "Macintosh|MacIntel|MacPPC|Mac68K" in
+  let ios_re = Regexp.regexp "iPhone|iPad|iPod" in
+  let windows_re = Regexp.regexp "Win32|Win64|Windows|WinCE" in
+  let android_re = Regexp.regexp "Android" in
+  let linux_re = Regexp.regexp "Linux" in
+
+  let matches_re re str = match Regexp.string_match re str 0 with
+    | Some _ -> true
+    | None -> false
+  in
+
+  List.find_map_exn
+    [ matches_re mac_re platform, Mac
+    ; matches_re ios_re platform, IOs
+    ; matches_re windows_re platform, Windows
+    ; matches_re android_re user_agent, Android
+    ; matches_re linux_re platform, Linux
+    ; true, Unknown
+    ]
+    ~f:(fun (matches, result) -> if matches then Some result else None)
+
 let is_enter key_evt =
   String.equal
     "Enter"
@@ -11,7 +44,29 @@ let is_meta key_evt = Js.to_bool key_evt##.metaKey
 let is_shift key_evt = Js.to_bool key_evt##.shiftKey
 let is_ctrl key_evt = Js.to_bool key_evt##.ctrlKey
 
-(** Is this an enter keypress plus meta, shift, or ctrl *)
+(** Is this an enter keypress plus meta, shift, or ctrl, depending on the platform *)
 let is_special_enter key_evt =
-  is_enter key_evt && (is_meta key_evt || is_shift key_evt || is_ctrl key_evt)
+  let platform = get_platform () in
+  let is_meta = is_meta key_evt in
+  let is_shift = is_shift key_evt in
+  let is_ctrl = is_ctrl key_evt in
+
+  let is_platform_special = match platform with
+    | Mac -> is_meta || is_shift
+    | Windows -> is_ctrl || is_shift
+    | Linux -> is_ctrl || is_shift
+    | Unknown -> is_ctrl || is_shift || is_meta
+    | IOs | Android -> false
+  in
+
+  is_enter key_evt && is_platform_special
 ;;
+
+let platform_special_combo ()
+  : [< Html_types.span_content_fun ] Js_of_ocaml_tyxml.Tyxml_js.To_dom.elt list option =
+  let open Js_of_ocaml_tyxml.Tyxml_js in
+  match get_platform () with
+  | IOs | Android -> None
+  | Mac -> Some [%html{|<kbd>⌘</kbd>-<kbd>Enter</kbd> or <kbd>Shift</kbd>-<kbd>Enter</kbd>|}]
+  | Windows -> Some [%html{|<kbd>⊞ Win</kbd>-<kbd>Enter</kbd> or <kbd>Shift</kbd>-<kbd>Enter</kbd>|}]
+  | Linux | Unknown -> Some [%html{|<kbd>Ctrl</kbd>-<kbd>Enter</kbd> or <kbd>Shift</kbd>-<kbd>Enter</kbd>|}]
