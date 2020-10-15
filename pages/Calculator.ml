d744cb66f2a59f19c18d8cfc842837e30573707a
Joel Burget <joelburget@gmail.com>
2020-10-14 17:57:57 -0700

    Add WIP language chart to calculator,

--- pages/Calculator.ml
+++ pages/Calculator.ml
@@ -1,6 +1,8 @@
 open Base
 open Lvca_syntax
 
+(* TODO: consider blocking asking for more than, say 5000 digits? *)
+
 module Model = struct
   type t = string * int
 
@@ -40,7 +42,42 @@ let mk_example str =
   Common.bind_event Common.Ev.clicks result_dom
     (fun _evt -> signal_event str; Lwt.return ());
 
-  result , click_event
+  result, click_event
+
+let language_chart =
+  let open Js_of_ocaml_tyxml.Tyxml_js in
+  [%html{|
+    <table class="language_chart">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Syntax</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr> <td>add</td> <td>expr + expr</td> <td></td> </tr>
+        <tr> <td>sub</td> <td>expr - expr</td> <td></td> </tr>
+        <tr> <td>mul</td> <td>expr * expr</td> <td></td> </tr>
+        <tr> <td>div</td> <td>expr / expr</td> <td></td> </tr>
+
+        <tr> <td>negate</td> <td>- expr</td> <td></td> </tr>
+        <tr> <td>sqrt</td> <td>sqrt expr</td> <td></td> </tr>
+        <tr> <td>abs</td> <td>abs expr</td> <td></td> </tr>
+        <tr> <td>exp</td> <td>exp expr</td> <td></td> </tr>
+        <tr> <td>ln</td> <td>ln expr</td> <td></td> </tr>
+        <tr> <td>sin</td> <td>sin expr</td> <td></td> </tr>
+        <tr> <td>cos</td> <td>cos expr</td> <td></td> </tr>
+        <tr> <td>tan</td> <td>tan expr</td> <td></td> </tr>
+        <tr> <td>asin</td> <td>asin expr</td> <td>arcsin ie inverse sin</td> </tr>
+        <tr> <td>acos</td> <td>acos expr</td> <td>arccos ie inverse cos</td> </tr>
+        <tr> <td>atan</td> <td>atan expr</td> <td>arctan ie inverse tan</td> </tr>
+
+        <tr> <td>pi</td> <td>pi</td> </tr>
+        <tr> <td>e</td> <td>e</td> </tr>
+      </tbody>
+    </table>
+  |}]
 
 module View = struct
   open Js_of_ocaml_tyxml.Tyxml_js
@@ -60,7 +97,7 @@ module View = struct
     let (_ : unit React.event) = input_event
       |> React.E.map (function
         | Common.InputUpdate str ->
-          Controller.update (Action.Evaluate str) model_s signal_update
+          Controller.update (Evaluate str) model_s signal_update
         | _ -> ())
     in
 
@@ -101,7 +138,7 @@ module View = struct
     let (_ : unit React.event) = example_update_es
       |> React.E.select
       |> React.E.map (fun example ->
-        Controller.update (Action.Evaluate example) model_s signal_update)
+        Controller.update (Evaluate example) model_s signal_update)
     in
 
     let examples = Html.(ul (examples
@@ -126,6 +163,8 @@ module View = struct
           <p>|}[ eval_button; instructions ]{|</p>
           <p>Try an example:</p>
           |}[ examples ]{|
+          <h3>language chart</h3>
+          |}[ language_chart ]{|
         </div>
       </div>
     |}]
