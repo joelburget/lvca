55e522d9a67febadc8170745c70420313c5e43eb
Joel Burget <joelburget@gmail.com>
2020-10-23 08:19:30 -0700

    Switch to signal pool model.

--- pages/Calculator.ml
+++ pages/Calculator.ml
@@ -3,34 +3,64 @@ open Lvca_syntax
 
 (* TODO: consider blocking asking for more than, say 5000 digits? *)
 
-module Model = struct
-  type evaluation = string * (int React.S.t * (?step:React.step -> int -> unit))
-  type t = evaluation list
+module IntMap = struct
+  type 'a t = (int, 'a, Base.Int.comparator_witness) Base.Map.t
+  let empty = Base.Map.empty (module Base.Int)
+end
 
-  let initial_model : t = []
+module Model = struct
+  type signal = int React.S.t * (?step:React.step -> int -> unit)
+  type signal_pool = signal IntMap.t
+  type evaluation = string * int
+  type t =
+    { evaluations: evaluation list
+    ; signal_pool: signal_pool
+    ; pool_idx: int
+    }
+
+  let initial_model : t =
+    { evaluations = []; signal_pool = IntMap.empty; pool_idx = 0 }
 end
 
 module Action = struct
   type t =
     | Evaluate of string
     | ChangePrecision of int * Common.digits_update
+    | DeleteRow of int
 end
 
+let rec remove_list_item list i = match list with
+  | [] -> list
+  | x :: xs -> if i = 0 then xs else x :: remove_list_item xs (i - 1)
+
 module Controller = struct
-  let update (action : Action.t) model_s signal_update =
-    let list = React.S.value model_s in
+  let update (action : Action.t) (model_s : Model.t React.S.t) signal_update =
+    let Model.{ evaluations; signal_pool; pool_idx } = React.S.value model_s in
     let new_model =
       match action with
-      | Evaluate str -> (str, React.S.create 10) :: list
+      | Evaluate str ->
+        Model.
+          { evaluations = (str, pool_idx) :: evaluations
+          ; signal_pool = Map.set signal_pool ~key:pool_idx ~data:(React.S.create 10)
+          ; pool_idx = Int.succ pool_idx
+          }
+      | DeleteRow i ->
+        let _, pool_idx = List.nth_exn evaluations i in
+
+        { evaluations = remove_list_item evaluations i
+        ; signal_pool = Map.remove signal_pool pool_idx
+        ; pool_idx
+        }
       | ChangePrecision (i, prec_cmd) ->
-        let _, (digits_s, digits_update) = List.nth_exn list i in
+        let _, pool_idx = List.nth_exn evaluations i in
+        let digits_s, digits_update = Map.find_exn signal_pool pool_idx in
         let () = match prec_cmd with
           | SetDigits digits -> digits_update digits
           (* TODO: there has to be a better way (than using value) *)
           | IncrDigits -> digits_update (React.S.value digits_s + 1)
           | DecrDigits -> digits_update (React.S.value digits_s - 1)
         in
-        list
+        { evaluations; signal_pool; pool_idx }
     in
     signal_update new_model
   ;;
@@ -83,6 +113,13 @@ let language_chart =
   |}]
 ;;
 
+let mk_button contents =
+  let open Js_of_ocaml_tyxml.Tyxml_js in
+  let click_event, signal_event = React.E.create () in
+  let handler _evt = signal_event (); false in
+  let button = Html.(button ~a:[a_onclick handler] contents) in
+  button, click_event
+
 module View = struct
   open Js_of_ocaml_tyxml.Tyxml_js
   module Ev = Js_of_ocaml_lwt.Lwt_js_events
@@ -90,6 +127,8 @@ module View = struct
 
   let view model_s signal_update =
     let input, input_event = Common.mk_input (React.S.const "1 + 1") in
+    (* TODO: shouldn't need to use input_dom *)
+    let input_dom = To_dom.of_textarea input in
 
     let (_ : unit React.event) =
       input_event
@@ -99,7 +138,15 @@ module View = struct
              | _ -> ())
     in
 
-    let eval_button = Html.(button [ txt "evaluate" ]) in
+    let eval_button, eval_event = mk_button Html.[ txt "evaluate" ] in
+
+    let (_ : unit React.event) = eval_event
+      |> React.E.map (fun () -> Controller.update
+        (Evaluate (Js_of_ocaml.Js.to_string input_dom##.value))
+        model_s
+        signal_update)
+    in
+
     let instructions =
       match WebUtil.platform_special_combo () with
       | None -> Html.txt ""
@@ -133,7 +180,7 @@ module View = struct
           ConstructiveReal.eval_to_string real ~digits)
     in
 
-    let row row_num input digits_s _digits_update =
+    let row row_num input_str digits_s _digits_update =
       let digits_entry, digits_event = Common.mk_digits_entry digits_s in
       let (_ : unit React.event) = digits_event
         |> React.E.map (fun update ->
@@ -141,27 +188,35 @@ module View = struct
       in
 
       let digits' = digits_s
-        |> React.S.map (result input)
+        |> React.S.map (result input_str)
         |> R.Html.txt
+      in
+
+      let delete_button, delete_event = mk_button Html.[ txt "delete" ] in
+      let (_ : unit React.event) = delete_event
+        |> React.E.map (fun () ->
+            Controller.update (DeleteRow row_num) model_s signal_update)
       in
 
       [%html{|
         <div class="row column-container">
           <div>
-            <pre>|}[ Html.txt input ]{|</pre>
+            <pre>|}[ Html.txt input_str ]{|</pre>
           </div>
           <div>
             <pre>|}[ digits' ]{|</pre>
           </div>
           <p>digits: |}[ digits_entry ]{|</p>
+          |}[ delete_button ]{|
         </div>
       |}]
     in
 
     let rows = model_s
-      |> React.S.map (fun q -> q
-        |> List.mapi ~f:(fun row_num (input, (digits_s, digits_update)) ->
-            row row_num input digits_s digits_update))
+      |> React.S.map (fun model -> model.Model.evaluations
+        |> List.mapi ~f:(fun row_num (input_str, pool_idx)  ->
+            let digits_s, digits_update = Map.find_exn model.signal_pool pool_idx in
+            row row_num input_str digits_s digits_update))
       |> ReactiveData.RList.from_signal
       |> R.Html.div
     in
@@ -169,12 +224,12 @@ module View = struct
     [%html {|
       <div>
         <h2>Calculator</h2>
+        <div>|}[ input ]{|</div>
+        <p>|}[ eval_button; instructions ]{|</p>
         <div class="row-container">
-          <div>|}[ input ]{|</div>
           |}[ rows ]{|
         </div>
         <div>
-          <p>|}[ eval_button; instructions ]{|</p>
           <p>Try an example:</p>
           |}[ examples ]{|
           <h3>language chart</h3>
