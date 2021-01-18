1ac1a519cfb81e1ab8ee8c0ba2265209fa15880c
Joel Burget <joelburget@gmail.com>
2021-01-18 09:03:31 -0800

    Remove open in mli.

--- core/Core.mli
+++ core/Core.mli
@@ -4,24 +4,23 @@
       [core_case_scope], (and [Pattern.t]).
     - [eval] is used to evaluate a core term *)
 
-open Lvca_syntax
-
 type is_rec =
   | Rec
   | NoRec
 
 type 'a term =
-  | Term of ('a, Primitive.t) Nominal.term
+  | Term of ('a, Lvca_syntax.Primitive.t) Lvca_syntax.Nominal.term
   | CoreApp of 'a * 'a term * 'a term
   | Case of 'a * 'a term * 'a core_case_scope list (** Cases match patterns *)
-  | Lambda of 'a * Sort.t * 'a core_scope
+  | Lambda of 'a * Lvca_syntax.Sort.t * 'a core_scope
       (** Lambdas bind variables. Patterns not allowed. *)
   | Let of 'a * is_rec * 'a term * 'a core_scope
       (** Lets bind variables. Patterns not allowed. *)
 
 and 'a core_scope = Scope of string * 'a term
 
-and 'a core_case_scope = CaseScope of ('a, Primitive.t) Pattern.t * 'a term
+and 'a core_case_scope =
+  | CaseScope of ('a, Lvca_syntax.Primitive.t) Lvca_syntax.Pattern.t * 'a term
 
 val map_loc : f:('a -> 'b) -> 'a term -> 'b term
 val erase : _ term -> unit term
@@ -32,12 +31,14 @@ val to_string : 'a term -> string
 type 'a eval_error = string * 'a term
 
 val eval_ctx
-  :  ('a, Primitive.t) Nominal.term Lvca_util.String.Map.t
+  :  ('a, Lvca_syntax.Primitive.t) Lvca_syntax.Nominal.term Lvca_util.String.Map.t
   -> 'a term
-  -> (('a, Primitive.t) Nominal.term, 'a eval_error) Base.Result.t
+  -> (('a, Lvca_syntax.Primitive.t) Lvca_syntax.Nominal.term, 'a eval_error) Base.Result.t
 
-val eval : 'a term -> (('a, Primitive.t) Nominal.term, 'a eval_error) Base.Result.t
+val eval
+  :  'a term
+  -> (('a, Lvca_syntax.Primitive.t) Lvca_syntax.Nominal.term, 'a eval_error) Base.Result.t
 
-module Parse (Comment : ParseUtil.Comment_int) : sig
-  val term : OptRange.t term ParseUtil.t
+module Parse (Comment : Lvca_syntax.ParseUtil.Comment_int) : sig
+  val term : Lvca_syntax.OptRange.t term Lvca_syntax.ParseUtil.t
 end
