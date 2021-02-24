179d0f3bf13bf82f22cbe7c83bc337777678a56b
Joel Burget <joelburget@gmail.com>
2021-02-24 07:49:25 -0800

    Update Path module.

--- syntax/Path.ml
+++ syntax/Path.ml
@@ -1,15 +1,13 @@
 open Base
 
 module T = struct
-  type t = (int * int) list
+  type t = int list
 
-  let compare = List.compare Lvca_util.Tuple2.Int.compare
-  let sexp_of_t = List.sexp_of_t Lvca_util.Tuple2.Int.sexp_of_t
+  let compare = List.compare Int.compare
+  let sexp_of_t = List.sexp_of_t Int.sexp_of_t
 end
 
 include T
 include Comparable.Make (T)
 
-let is_prefix ~path ~prefix =
-  List.is_prefix path ~prefix ~equal:Lvca_util.Tuple2.Int.( = )
-;;
+let is_prefix ~path ~prefix = List.is_prefix path ~prefix ~equal:Int.( = )
--- syntax/Path.mli
+++ syntax/Path.mli
@@ -1,12 +1,11 @@
-(** A path points to some subterm. At each step we point to a slot and an index within
-    that slot. For example [foo(bar(a, b; c))]
+(** A path (sometimes called "occurrence") points to some subterm. At each step we point
+    to a subterm. For example [foo(a; bar(b; c))].
 
-    - [\[\]] points to [foo(bar(a, b; c))]
-    - [\[0, 0\]] points to [bar(a, b; c)]
-    - [\[0, 0; 0, 0\]] points to [a]
-    - [\[0, 0; 0, 1\]] points to [b]
-    - [\[0, 0; 1, 0\]] points to [c] *)
-type t = (int * int) list
+    - [\[\]] points to [foo(a; bar(b; c))]
+    - [\[0\]] points to [a]
+    - [\[1; 0\]] points to [b]
+    - [\[1; 1\]] points to [c] *)
+type t = int list
 
 type comparator_witness
 
