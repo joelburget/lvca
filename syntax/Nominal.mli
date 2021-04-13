d20e7261dec392aa831407480b19690e42c78564
Joel Burget <joelburget@gmail.com>
2021-04-13 07:19:28 -0700

    Testing Nominal fix.

--- syntax/Nominal.mli
+++ syntax/Nominal.mli
@@ -1,4 +1,5 @@
 (** Representation of terms that simply uses variable names to represent scope. *)
 
-module Make (Prim : LanguageObject_intf.S) :
-  Nominal_intf.S with module Prim = Prim and module Pat = Pattern.Make(Prim)
+module Make (Prim : LanguageObject_intf.S) : Nominal_intf.S
+(* with module OuterPrim = Prim *)
+(* and module OuterPat = Pattern.Make(Prim) *)
