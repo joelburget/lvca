389da436f0404127aa4ca1e6ab193d09403752a2
Joel Burget <joelburget@gmail.com>
2021-04-22 17:18:17 -0700

    Remove unused Document store stuff.

--- languages/Document.ml
+++ languages/Document.ml
@@ -1,90 +1,13 @@
 open Base
-open Lvca_bidirectional
-open Lvca_core
 open Lvca_syntax
 open Omd
 open Stdio
 module Util = Lvca_util
 
-module Store = struct
-  type store_value =
-    (* abstract syntaxes *)
-    | GenesisTermLanguage
-    | GenesisAbstractSyntaxLanguage
-    | GenesisConcreteSyntaxLanguage
-    | GenesisDynamicsLanguage
-    | GenesisStaticsLanguage
-    (* concrete syntaxes *)
-    | GenesisTermConcrete
-    | GenesisAbstractSyntaxConcrete
-    | GenesisConcreteSyntaxConcrete
-    | GenesisDynamicsConcrete
-    | GenesisStaticsConcrete
-    | Term of unit Nominal.Term.t
-
-  type term_store = unit Nominal.Term.t Util.String.Map.t
-  type name_store = string Util.String.Map.t
-
-  type t =
-    { term_store : term_store
-    ; name_store : name_store
-    }
-
-  let lookup_sha : term_store -> string -> store_value =
-   fun term_store -> function
-    (* abstract syntaxes *)
-    | "0000000000000000000000000000000000000000000000000000000000000001" ->
-      GenesisTermLanguage
-    | "0000000000000000000000000000000000000000000000000000000000000002" ->
-      GenesisAbstractSyntaxLanguage
-    | "0000000000000000000000000000000000000000000000000000000000000003" ->
-      GenesisConcreteSyntaxLanguage
-    | "0000000000000000000000000000000000000000000000000000000000000004" ->
-      GenesisDynamicsLanguage
-    | "0000000000000000000000000000000000000000000000000000000000000005" ->
-      GenesisStaticsLanguage
-    (* concrete syntaxes *)
-    | "0000000000000000000000000000000000000000000000000000000000000006" ->
-      GenesisTermConcrete
-    | "0000000000000000000000000000000000000000000000000000000000000007" ->
-      GenesisAbstractSyntaxConcrete
-    | "0000000000000000000000000000000000000000000000000000000000000008" ->
-      GenesisConcreteSyntaxConcrete
-    | "0000000000000000000000000000000000000000000000000000000000000009" ->
-      GenesisDynamicsConcrete
-    | "000000000000000000000000000000000000000000000000000000000000000a" ->
-      GenesisStaticsConcrete
-    | sha_str -> Term (Map.find_exn term_store sha_str)
- ;;
-
-  let initial_name_store : name_store =
-    Util.String.Map.of_alist_exn
-      (* abstract syntaxes *)
-      [ "term", "0000000000000000000000000000000000000000000000000000000000000001"
-      ; ( "abstract_syntax"
-        , "0000000000000000000000000000000000000000000000000000000000000002" )
-      ; ( "concrete_syntax"
-        , "0000000000000000000000000000000000000000000000000000000000000003" )
-      ; "dynamics", "0000000000000000000000000000000000000000000000000000000000000004"
-      ; "statics", "0000000000000000000000000000000000000000000000000000000000000005"
-        (* concrete syntaxes *)
-      ; ( "term_concrete"
-        , "0000000000000000000000000000000000000000000000000000000000000006" )
-      ; ( "abstract_syntax_concrete"
-        , "0000000000000000000000000000000000000000000000000000000000000007" )
-      ; ( "concrete_syntax_concrete"
-        , "0000000000000000000000000000000000000000000000000000000000000008" )
-      ; ( "dynamics_concrete"
-        , "0000000000000000000000000000000000000000000000000000000000000009" )
-      ; ( "statics_concrete"
-        , "000000000000000000000000000000000000000000000000000000000000000a" )
-      ]
-  ;;
-end
-
 let commands_abstract_syntax =
   {|
-{ string, maybe }
+string : *
+maybe : * -> *
 
 // maybe(a) := nothing() | just(a)
 
@@ -105,135 +28,10 @@ command :=
   |}
 ;;
 
-(* let commands_concrete_syntax = let str = {| DEFINE := "define" EVAL := "eval" COLON :=
-   ":" LANGLE := "<" RANGLE := ">" SHA := /[0-9a-f]{64}/ IDENT := /[0-9a-zA-Z_]+/ STRING
-   := /\{\{.*\}\}/
-
-   sha_or_name := | sha = SHA { sha(string(sha)) } | ident = IDENT { name(string(ident)) }
-
-   language := | abstract = sha_or_name LANGLE concrete = sha_or_name RANGLE {
-   language(abstract; just(concrete)) } | abstract = sha_or_name { language(abstract;
-   nothing()) }
-
-   command := | DEFINE ident = IDENT COLON lang = language { define(just(string(ident));
-   lang) } | EVAL sha_or_name = sha_or_name lang = language { eval(sha_or_name; lang) } |}
-   in match Parse_concrete.parse str with | Error err -> failwith (ParseError.to_string
-   err) | Ok (pre_terminal_rules, sort_rules) -> let desc =
-   ConcreteSyntax.make_concrete_description pre_terminal_rules sort_rules in (match
-   ConcreteSyntax.check_description_validity desc with | Some (InvalidGrammar msg) ->
-   failwith msg | None -> desc) ;;
-
-   let parse_command : string -> (NonBinding.term, string) Result.t = fun str ->
-   printf "parsing command: '%s'\n" str; let str' = String.slice str 1 (-1) in
-   match ConcreteSyntax.parse commands_concrete_syntax "command" str' with | Error _err ->
-   Error (* (ParseError.to_string err) *) "parse error" | Ok tree -> match
-   ConcreteSyntax.to_ast commands_concrete_syntax tree with | Error msg -> Error msg | Ok
-   ast -> match NonBinding.from_nominal ast with | None -> Error "Failed to convert ast to
-   non-binding" | Some tm -> Ok tm *)
-
-let lookup_lang : Store.t -> unit NonBinding.term -> Store.store_value =
- fun { term_store; name_store } -> function
-  | Operator ((), "sha", [ Primitive ((), String sha_str) ]) ->
-    Store.lookup_sha term_store sha_str
-  | Operator ((), "name", [ Primitive ((), String name) ]) ->
-    Map.find_exn name_store name |> Store.lookup_sha term_store
-  | tm ->
-    Fmt.failwith
-      "Failed to look up language term %a"
-      Nominal.Term.pp
-      (NonBinding.to_nominal tm)
-;;
-
-let term_of_maybe : unit NonBinding.term -> unit NonBinding.term option = function
-  | Operator ((), "just", [ a ]) -> Some a
-  | Operator ((), "nothing", []) -> None
-  | _ -> failwith "term_of_maybe: unexpected term"
-;;
-
-let lookup_maybe_concrete : Store.t -> unit NonBinding.term -> Store.store_value =
- fun store tm ->
-  Fmt.pr "lookup_maybe_concrete tm: %a\n" Nominal.Term.pp (NonBinding.to_nominal tm);
-  let tm' = term_of_maybe tm in
-  match tm' with
-  | None -> GenesisTermConcrete
-  | Some tm'' ->
-    Fmt.pr "lookup_maybe_concrete tm'': %a\n" Nominal.Term.pp (NonBinding.to_nominal tm'');
-    lookup_lang store tm''
-;;
-
-type parsed =
-  | ParsedTerm of unit Nominal.Term.t
-  | ParsedAbstract of unit AbstractSyntax.t
-  | ParsedStatics of unit Statics.TypingRule.t list
-  | ParsedDynamics of unit Core.term
-
-(*
-let term_of_parsed : parsed -> unit Nominal.term = function
-  | ParsedTerm tm -> tm
-  | ParsedAbstract abstract_syntax ->
-    abstract_syntax |> AbstractSyntax.to_term |> NonBinding.to_nominal
-  | ParsedStatics rules -> Statics.to_term rules
-  | ParsedDynamics dynamics -> Dynamics.Core.to_term dynamics
-;;
-
-let parse_store_value : Store.store_value -> Store.store_value -> string -> parsed =
- fun _abstract_syntax_val concrete_syntax_val str ->
-  let lex = Lexing.from_string str in
-  printf "Parsing {|%s|}\n" str;
-  match concrete_syntax_val with
-  | GenesisTermConcrete -> ParsedTerm (Term.Parser.top_term Term.Lexer.read lex)
-  | GenesisAbstractSyntaxConcrete ->
-    ParsedAbstract (AbstractSyntax.Parser.language_def AbstractSyntax.Lexer.read lex)
-  | GenesisStaticsConcrete -> ParsedStatics (Statics.Parser.rules Statics.Lexer.read lex)
-  | GenesisDynamicsConcrete ->
-    ParsedDynamics (Dynamics.Parser.dynamics Dynamics.Lexer.read lex)
-  | Term concrete_syntax_tm ->
-    (match NonBinding.from_nominal concrete_syntax_tm with
-    | None -> failwith "TODO 1"
-    | Some concrete_syntax_tm' ->
-      let concrete_syntax = ConcreteSyntaxDescription.of_term concrete_syntax_tm' in
-      let ast =
-        match ConcreteSyntax.parse concrete_syntax "tm" (* XXX root name*) str with
-        | Error _err -> Error (* (ParseError.to_string err) *) "parse error"
-        | Ok tree -> ConcreteSyntax.to_ast concrete_syntax tree
-      in
-      (match ast with Ok ast -> ParsedTerm ast | Error msg -> failwith msg))
-  | _ -> failwith "TODO 2"
-;;
-
-(* | Parseable { abstract_syntax = _; concrete_syntax } -> (match concrete_syntax with |
-   None -> Term.Parser.top_term Term.Lexer.read lex | Some concrete_syntax' -> ) | _ ->
-   failwith "TODO" *)
-
-(* TODO: we end up double-wrapping the results of this call in eval_inline_block *)
-let eval_command : Store.t -> unit NonBinding.term -> string -> Vdom.Node.t =
- fun ({ term_store; name_store } as store) cmd body ->
-  match cmd with
-  | Operator
-      ("define", [ maybe_ident; Operator ("language", [ abstract_tm; concrete_tm ]) ]) ->
-    let lang_val = lookup_lang store abstract_tm in
-    let concrete_val = lookup_maybe_concrete store concrete_tm in
-    let parsed_defn = parse_store_value lang_val concrete_val body in
-    let defn_tm = term_of_parsed parsed_defn in
-    let key = Binding.Nominal.hash defn_tm in
-    Map.set term_store ~key ~data:defn_tm;
-    (match maybe_ident with
-    | Operator ("just", [ Primitive (String ident) ]) ->
-      printf "setting name store %s -> %s\n" ident key;
-      Map.set name_store ~key:ident ~data:key
-    | _ ->
-      printf "not setting name store (%s)\n" key;
-      ());
-    (* TODO: structured *)
-    Vdom.Node.(pre [] [ code [] [ text @@ Binding.Nominal.pp_term' defn_tm ] ])
-  | Operator ("eval", [ _ident ]) -> failwith "TODO eval"
-  | _ -> failwith "TODO unknown command"
-;;
-*)
-
-let abstractSyntax =
+let abstract_syntax =
   {|
-// global: list, string
+list : * -> *
+string : *
 
 option(a) := some(a) | none()
 
@@ -437,16 +235,16 @@ let evaluate_and_produce_dom : store -> string -> Brr.El.t =
 
 let parse : string -> (unit NonBinding.term, string) Result.t =
  fun str ->
-  let md = Omd.of_string str in
-  try Ok (mk_sequence (List.map md ~f:term_of_inline_block)) with
+  try Ok (str |> Omd.of_string |> List.map ~f:term_of_inline_block |> mk_sequence) with
   | TranslationError msg -> Error msg
 ;;
 
 let%test_module "markdown" =
   (module struct
     let print_parse str =
-      print_string
-        (match parse str with Ok tm -> Fmt.str "%a" NonBinding.pp tm | Error msg -> msg)
+      match parse str with
+      | Ok tm -> Fmt.pr "%a" NonBinding.pp tm
+      | Error msg -> print_string msg
     ;;
 
     let%expect_test _ =
