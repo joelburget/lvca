6076a3bb6ed2f5b89e624ad5af43dcd48daddd16
Joel Burget <joelburget@gmail.com>
2021-06-21 07:54:16 -0700

    Simplify DPCF: remove transition_result datatype.

--- languages/Pfpl_dpcf.ml
+++ languages/Pfpl_dpcf.ml
@@ -1,4 +1,5 @@
 open Base
+open Result.Let_syntax
 
 module Mk_lang =
 [%lvca.abstract_syntax_module
@@ -45,46 +46,39 @@ let rec subst v name exp =
   | Exp_var (_info, name') -> if String.(name = name') then v else exp
 ;;
 
-type 'info transition_result =
-  | Transitioned of 'info Lang.Types.exp
-  | Stuck of 'info Lang.Types.exp
-  | Error of string * 'info Lang.Types.exp
-
 let rec transition tm =
   match tm with
-  | Lang.Types.Zero info ->
-    Transitioned (Lang.Types.Num (info, (info, Z.zero))) (* 22.4a *)
+  | Lang.Types.Zero info -> Ok (Lang.Types.Num (info, (info, Z.zero))) (* 22.4a *)
   | Succ (info, d) ->
     (match d with
     | Lang.Types.Num (_, (info, z)) ->
       (* 22.4d *)
-      Transitioned (Lang.Types.Num (info, (info, Z.(z + one))))
+      Ok (Lang.Types.Num (info, (info, Z.(z + one))))
     | _ ->
       (match transition d with
       (* 22.4b, c *)
-      | Transitioned d -> Transitioned (Lang.Types.Succ (info, d))
+      | Ok d -> Ok (Lang.Types.Succ (info, d))
       | other -> other))
   | Ifz (info, d0, (x, d1), d) ->
     (* 22.4f-j *)
     (match d with
     | Lang.Types.Num (_, (info, z)) ->
       if Z.(equal z zero)
-      then Transitioned d0 (* 22.4h *)
-      else Transitioned (subst (Num (info, (info, Z.pred z))) x d1) (* 22.4i *)
+      then Ok d0 (* 22.4h *)
+      else Ok (subst (Num (info, (info, Z.pred z))) x d1) (* 22.4i *)
     | _ ->
-      (match transition d with
-      | Transitioned d -> Transitioned (Ifz (info, d0, (x, d1), d))
-      | other -> other))
+      (match transition d with Ok d -> Ok (Ifz (info, d0, (x, d1), d)) | other -> other))
   | Ap (info, d1, d2) ->
     (* 22.4k-n *)
     (match d1 with
-    | Fun (_, (x, d)) -> (* 22.4m *) Transitioned (subst d2 x d)
+    | Fun (_, (x, d)) -> (* 22.4m *) Ok (subst d2 x d)
     | _ ->
       (match transition d1 with
-      | Transitioned d1 -> Transitioned (Lang.Types.Ap (info, d1, d2))
+      | Ok d1 -> Ok (Lang.Types.Ap (info, d1, d2))
       | other -> other))
-  | Fix (_info, (x, d)) -> Transitioned (subst tm x d) (* 22.4o *)
-  | _ -> Stuck tm
+  | Fix (_info, (x, d)) -> Ok (subst tm x d) (* 22.4o *)
+  | Lang.Types.Num _ -> Ok tm
+  | _ -> Error ("stuck", tm)
 
 and err = function
   | Lang.Types.Succ (_, d) -> err d (* 22.4c *) || isnt_num d (* 22.4e *)
@@ -97,10 +91,8 @@ let rec eval tm =
   if is_val tm
   then Ok tm
   else (
-    match transition tm with
-    | Transitioned tm -> eval tm
-    | Stuck tm -> if is_val tm then Ok tm else Error ("stuck", tm)
-    | Error (msg, tm) -> Error (msg, tm))
+    let%bind tm = transition tm in
+    eval tm)
 ;;
 
 let%test_module _ =
