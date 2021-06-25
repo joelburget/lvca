2781a7222bbe2c3cac0763b9159396c95c4d1f99
Joel Burget <joelburget@gmail.com>
2021-06-25 08:29:45 -0700

    Simplify handling of PCF intermediate states.

--- pages/Pcf.ml
+++ pages/Pcf.ml
@@ -30,7 +30,6 @@ let parsed_to_result = function
 module Model = struct
   type t =
     { input : string
-    ; parsed_input : (term, string) Result.t
     ; intermediate_results : term list
     ; result : (term, string * term option) Result.t
     ; ranges : Ranges.t
@@ -38,15 +37,11 @@ module Model = struct
 
   let initial_model =
     let input = "Succ(Ifz(Zero(); x. x; Zero()))" in
-    let parsed_input = parse input in
-    let intermediate_results, result = parsed_to_result parsed_input in
-    { input; parsed_input; intermediate_results; result; ranges = [] }
+    let intermediate_results, result = input |> parse |> parsed_to_result in
+    { input; intermediate_results; result; ranges = [] }
   ;;
 
-  let pp ppf { input; parsed_input; intermediate_results; result; ranges } =
-    let pp_parsed_input ppf tm_result =
-      match tm_result with Error msg -> Fmt.string ppf msg | Ok tm -> Exp.pp ppf tm
-    in
+  let pp ppf { input; intermediate_results; result; ranges } =
     let pp_tm_result ppf tm_result =
       match tm_result with
       | Error (msg, Some tm) -> Fmt.(pair string Exp.pp) ppf (msg, tm)
@@ -55,11 +50,8 @@ module Model = struct
     in
     Fmt.pf
       ppf
-      "{ input = %s; parsed_input = %a; intermediate_results = %a; result = %a; ranges = \
-       %a }"
+      "{ input = %s; intermediate_results = %a; result = %a; ranges = %a }"
       input
-      pp_parsed_input
-      parsed_input
       Fmt.(list Exp.pp)
       intermediate_results
       pp_tm_result
@@ -70,12 +62,10 @@ module Model = struct
 
   let ( = ) m1 m2 =
     let exp_eq = Exp.equal ~info_eq:(Provenance.equal ~info_eq:Opt_range.( = )) in
-    let parsed_input_eq = Result.equal exp_eq String.( = ) in
     let result_eq =
       Result.equal exp_eq (Lvca_util.Tuple2.equal String.( = ) (Option.equal exp_eq))
     in
     String.(m1.input = m2.input)
-    && parsed_input_eq m1.parsed_input m2.parsed_input
     && result_eq m1.result m2.result
     && Ranges.( = ) m1.ranges m2.ranges
   ;;
@@ -91,9 +81,8 @@ module Controller = struct
   let update (action : Action.t) model =
     match action with
     | Evaluate input ->
-      let parsed_input = parse input in
-      let intermediate_results, result = parsed_to_result parsed_input in
-      Model.{ input; parsed_input; intermediate_results; result; ranges = [] }
+      let intermediate_results, result = input |> parse |> parsed_to_result in
+      Model.{ input; intermediate_results; result; ranges = [] }
     | SetInputHighlights ranges -> { model with ranges }
   ;;
 end
@@ -132,18 +121,6 @@ module View = struct
              | InputUpdate _ -> Some (SetInputHighlights [])
              | _ -> None)
     in
-    let set_highlight_e1, input_tree_view =
-      let s =
-        model_s
-        |> S.map
-             ~eq:(Lvca_util.Tuple2.equal phys_equal phys_equal)
-             (fun Model.{ parsed_input; _ } ->
-               match parsed_input with
-               | Error msg -> E.never, div [ txt msg ]
-               | Ok tm -> mk_tree_view tm)
-      in
-      S.Pair.fst ~eq:phys_equal s, S.Pair.snd ~eq:phys_equal s
-    in
     let set_highlight_e2, intermediate_tree_views =
       let s =
         model_s
@@ -151,11 +128,7 @@ module View = struct
              ~eq:(Lvca_util.Tuple2.equal phys_equal phys_equal)
              (fun Model.{ intermediate_results; _ } ->
                let events, elems =
-                 intermediate_results
-                 (* XXX change intermediate results to not include first step or remove input_tree_view above *)
-                 |> List.tl_exn
-                 |> List.map ~f:mk_tree_view
-                 |> List.unzip
+                 intermediate_results |> List.map ~f:mk_tree_view |> List.unzip
                in
                E.select events, elems)
       in
@@ -174,17 +147,11 @@ module View = struct
       S.Pair.fst ~eq:phys_equal s, S.Pair.snd ~eq:phys_equal s
     in
     let actions =
-      E.select
-        [ E.swap set_highlight_e1
-        ; E.swap set_highlight_e2
-        ; E.swap set_highlight_e3
-        ; enter_input_e
-        ]
+      E.select [ E.swap set_highlight_e2; E.swap set_highlight_e3; enter_input_e ]
     in
     let elem =
       div
         [ input_elem
-        ; mk_reactive' div input_tree_view
         ; mk_reactive div intermediate_tree_views
         ; mk_reactive' div output_tree_view
         ]
