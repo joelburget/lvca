18e69eb6367a1cfd957595e51881555f535a5c42
Joel Burget <joelburget@gmail.com>
2021-08-13 08:25:41 -0700

    Use / set path in devel_main.

--- pages/devel_main.ml
+++ pages/devel_main.ml
@@ -21,25 +21,43 @@ module Model = struct
     | Repl_page
     | Ide_page
 
-  type t = { page : page }
+  let ( = ) p1 p2 =
+    match p1, p2 with
+    | Term_and_concrete_page, Term_and_concrete_page
+    | Calculator_page, Calculator_page
+    | Eval_with_provenance_page, Eval_with_provenance_page
+    | Term_to_tex_page, Term_to_tex_page
+    | Parser_page, Parser_page
+    | Scope_viewer_page, Scope_viewer_page
+    | Edits_page, Edits_page
+    | Check_term_page, Check_term_page
+    | Ast_operations_page, Ast_operations_page
+    | List_nat_page, List_nat_page
+    | Pcf_page, Pcf_page
+    | Term_and_document_page, Term_and_document_page
+    | Repl_page, Repl_page
+    | Ide_page, Ide_page ->
+      true
+    | _, _ -> false
+  ;;
 
-  let initial_model = { page = Repl_page }
+  let default_page = Repl_page
 
   let all_pages =
-    [ Term_and_concrete_page
-    ; Calculator_page
-    ; Parser_page
-    ; Scope_viewer_page
-    ; Eval_with_provenance_page
-    ; Term_to_tex_page
-    ; Edits_page
-    ; Check_term_page
-    ; Ast_operations_page
-    ; List_nat_page
-    ; Pcf_page
-    ; Term_and_document_page
-    ; Repl_page
-    ; Ide_page
+    [ Term_and_concrete_page, "term-and-concrete"
+    ; Calculator_page, "calculator"
+    ; Parser_page, "parser"
+    ; Scope_viewer_page, "scope-viewer"
+    ; Eval_with_provenance_page, "eval-with-provenance"
+    ; Term_to_tex_page, "term-to-tex"
+    ; Edits_page, "edits"
+    ; Check_term_page, "check-term"
+    ; Ast_operations_page, "ast-operations"
+    ; List_nat_page, "list-nat"
+    ; Pcf_page, "pcf"
+    ; Term_and_document_page, "term-and-document"
+    ; Repl_page, "repl"
+    ; Ide_page, "ide"
     ]
   ;;
 end
@@ -86,11 +104,11 @@ module View = struct
     let page_selector =
       Ui.Value_selector.Menu.v
         Lvca_util.(page_description >> Jstr.v)
-        (S.const Model.all_pages)
-        (S.map (fun model -> model.Model.page) model_s)
+        (Model.all_pages |> List.map ~f:fst |> S.const)
+        model_s
     in
     let page_view =
-      mk_reactive div (model_s |> S.map (fun { page } -> [ stateless_view page () ]))
+      mk_reactive div (model_s |> S.map (fun page -> [ stateless_view page () ]))
     in
     let elems =
       [ main
@@ -112,13 +130,41 @@ end
 let main () =
   let open Fut.Syntax in
   let* _ev = Ev.next Ev.load (Window.as_target G.window) in
+  let initial_uri = G.window |> Window.location in
   let wrapper model_s =
     let evts, children = View.view model_s in
-    let do_action = E.map (fun page _model -> Model.{ page }) evts in
+    let do_action =
+      E.map
+        (fun page _model ->
+          let page_name =
+            match
+              List.find Model.all_pages ~f:(fun (page', _) -> Model.(page' = page))
+            with
+            | None -> assert false
+            | Some (_, name) -> name
+          in
+          let path = Jstr.v ("/" ^ page_name ^ "/") in
+          (match Uri.with_uri ~path initial_uri with
+          | Ok uri -> Window.set_location G.window uri
+          | Error _ -> ());
+          page)
+        evts
+    in
     let model_s' = S.accum (S.value model_s) do_action in
     model_s', (model_s', children)
   in
-  let model_s, children = S.fix Model.initial_model wrapper in
+  let path_components =
+    initial_uri |> Uri.path |> Jstr.to_string |> String.split ~on:'/'
+  in
+  let page =
+    match path_components with
+    | [ ""; pc_name; "" ] ->
+      (match List.find Model.all_pages ~f:(fun (_, name) -> String.(name = pc_name)) with
+      | None -> Model.default_page
+      | Some (page, _) -> page)
+    | _ -> Model.default_page
+  in
+  let model_s, children = S.fix page wrapper in
   Logr.hold (S.log model_s (fun _ -> ()));
   (match Document.find_el_by_id G.document (Jstr.v "app") with
   | None -> assert false
